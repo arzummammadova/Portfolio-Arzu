66a0278d0f65dd094c62627ef4b7c6ce1c3ee309
arzummammadova <arzuam-af106@code.edu.az>
2025-07-28 02:18:33 +0400

    new favoritespage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,18 @@
 'use client';
+import { useEffect } from 'react';
 import Education from "@/components/Education";
 import Hero from "@/components/Hero";
+
 export default function Home() {
+  
+
   return (
     <div>
+      <div className="custom-cursor"></div>
+
       <Hero />
 
-      {/* <AboutMe /> */}
-      <Education/>
+      <Education />
     </div>
   );
 }
-
